62d0aabf7960c2a742b04c3104e1fe1c0b31ed1b
BinkyTwin <abdelatifdjeddou@gmail.com>
2025-01-08 16:11:08 +0100

    Rajout faction + faction du joueur + point de vie du joueur + points d'expérience du joueur

--- game/player.py
+++ game/player.py
@@ -38,14 +38,36 @@ class Player:
             'diplomatie': 6
         }
     }
+    FACTIONS = {
+        "Les Veilleurs des Montagnes",
+        "Le Cercle des Ombres",
+        "Le Clan des Brumes",
+        "Les Enfants de la Forêt"
+    }
 
-    def __init__(self, name, x, y, race):
+    def __init__(self, name, x, y, race, faction):
+        # nom du joueur
         self.name = name
+
+        # position du joueur
         self.x = x
         self.y = y
+
+        # race du joueur
         if race not in self.RACES:
             raise ValueError(f"Race invalide. Choisissez parmi : {', '.join(self.RACES.keys())}")
         self.race = race
-        #self.stats = self.RACES[race].copy()
-        self.stats = self.RACES[race].copy()
+        self.race_stats = self.RACES[race].copy() # stats de la race
+
+        # Faction du joueur
+        if faction not in self.FACTIONS:
+            raise ValueError(f"Faction invalide. Choisissez parmi : {', '.join(self.FACTIONS)}")
+        self.faction = faction
+        self.faction_stats = self.FACTIONS[faction].copy() # stats de la faction
+
+        # points de vie du joueur
+        self.hp = 100
+
+        # points d'expérience du joueur
+        self.xp = 0
 
