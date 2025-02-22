278a95c5958a5ab123a8c16d29a531a3c920a7c3
BinkyTwin <abdelatifdjeddou@gmail.com>
2025-02-22 23:40:55 +0100

    Merge de la branche

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\manis\Documents\Python\RPG\rpg-planete-singes\venv
+set VIRTUAL_ENV=c:\Users\djedd\Desktop\EMLV\Python\RPG\rpg-planete-singes\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
